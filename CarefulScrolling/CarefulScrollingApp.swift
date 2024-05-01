98a89c11168c032e7c8667ca24fa98bdeecd22ed
Andrew Gogol <andrewgogol777@gmail.com>
2024-05-01 20:12:05 +0300

    Initial Commit

---
+++ CarefulScrolling/CarefulScrollingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarefulScrollingApp.swift
+//  CarefulScrolling
+//
+//  Created by admin on 01.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct CarefulScrollingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
