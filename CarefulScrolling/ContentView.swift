bf9fffb3ae1671dd08bb643cdfcacb46f6207951
Andrew Gogol <andrewgogol777@gmail.com>
2024-05-02 12:56:05 +0300

    setup scroll view and tab view

--- CarefulScrolling/ContentView.swift
+++ CarefulScrolling/ContentView.swift
@@ -8,17 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var computedHeight: CGFloat {
+        50
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            ExtractedScrollView(height: computedHeight)
+                .tabItem {
+                    Image(systemName: "star.fill")
+                    Text("First")
+                }
         }
-        .padding()
+        .onAppear() {
+            UITabBar.appearance().barTintColor = .white
+        }
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct ExtractedScrollView: View {
+    var height: CGFloat
+    
+    var body: some View {
+        ZStack {
+            ScrollView {
+                VStack {
+                    ForEach(0..<101) {
+                        Text("\($0)")
+                            .font(.monospaced(.title2)())
+                            .foregroundStyle(.secondary)
+                            .frame(maxWidth: .infinity)
+                    }
+                }
+            }
+            .contentMargins(.bottom, height, for: .scrollContent)
+            .contentMargins(.bottom, height, for: .scrollIndicators)
+            VStack {
+                Spacer()
+                Rectangle()
+                    .frame(height: height)
+                    .foregroundStyle(.red)
+                    .opacity(0.4)
+            }
+        }
+    }
 }
